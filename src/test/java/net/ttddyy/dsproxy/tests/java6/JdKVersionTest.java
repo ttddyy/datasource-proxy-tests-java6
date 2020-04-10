164943169157c381e47bdaa94fa1bea35578d58a
Tadaya Tsuyukubo <tadaya@ttddyy.net>
2020-04-10 10:18:22 -0700

    Skip JdkVersionTest invoked by surefire

--- src/test/java/net/ttddyy/dsproxy/tests/java6/JdKVersionTest.java
+++ src/test/java/net/ttddyy/dsproxy/tests/java6/JdKVersionTest.java
@@ -3,6 +3,7 @@ package net.ttddyy.dsproxy.tests.java6;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assume.assumeFalse;
 
 /**
  *
@@ -12,7 +13,21 @@ public class JdKVersionTest {
 
 	@Test
 	public void jdkVersionCheck() {
+
 		String version = System.getProperty("java.specification.version");
+
+		boolean isSurefire = "surefire".equalsIgnoreCase(System.getProperty("test.mode"));
+		if (isSurefire) {
+			// since mvn ran with jdk 1.7, skip this test. see surefire config in pom.xml.
+			System.out.println();
+			System.out.println("***********************************************");
+			System.out.println("**  SKIPPING JDK Version Check Test (JDK=" + version + ")");
+			System.out.println("***********************************************");
+			System.out.println();
+		}
+
+		assumeFalse("Skip invoked by surefire", isSurefire);
+
 		assertEquals("1.6", version);
 	}
 
